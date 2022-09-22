41af00bb986bfe576dc402acaf6f3204bbe37c55
azeddine <maslouhazeddine@gmail.com>
2022-09-22 15:11:27 +0100

    done 40%

--- main.js
+++ main.js
@@ -3,11 +3,29 @@ var turn = 'X';
 function game(id) {
 
     let elemen = document.getElementById(id);
-
     if (turn === 'X' && elemen.innerHTML == '') {
-
+        playersName.innerHTML = (localStorage.getItem("player2") + "O");
         elemen.innerHTML = 'X';
-        turn = 'X';
+        turn = 'O';
 
+    } else if (turn === 'O' && elemen.innerHTML == '') {
+        playersName.innerHTML = (localStorage.getItem("player1") + "X");
+        elemen.innerHTML = 'O';
+        turn = 'X';
     }
+}
+
+function player() {
+
+    localStorage.setItem('player1', player1.value);
+    localStorage.setItem('player2', player2.value);
+    document.getElementById("player").style.display = "none"
+    playersName.innerHTML = (localStorage.getItem("player1") + "X");
+
+}
+
+
+function reload() {
+    location.reload();
+    localStorage.clear();
 }
